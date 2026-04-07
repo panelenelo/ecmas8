f87e6c82fb6c3a7398081c8128a8d9c0bd3692cf
panelenelo <panelenelo@gmail.com>
2026-04-07 18:29:44 -0300

    made the counter

---
+++ scripts/counter.js
@@ -0,0 +1,27 @@
+
+let count=0;
+let countEl=document.getElementById("count-el");
+
+console.log(countEl);
+
+function increment(){
+    count++;
+    countEl.innerText=count;
+}
+
+function decrement(){
+    count--;
+    if(count<0){
+        count=0;
+    }
+    countEl.innerText=count;
+}
+
+function reset(){
+    count=0;
+    countEl.innerText=count;
+}
+
+function save(){
+    console.log(count);
+}
